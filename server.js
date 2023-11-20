6b48e23b53cafaa42c754faa8eb120bd82a8959f
EthanVB123 <ethanvanbruchem@gmail.com>
2023-11-20 14:44:27 +0800

    That's enough for now (t=29:45)

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const app = express()
-
+app.use(express.static("public")) // folder for static html etc. files
 app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => { // When searching the root directory, do this
+app.use(logger) // happens every time because at the top of the file
+app.get('/',/*logger,*/ (req, res) => { // When searching the root directory, do this
     console.log('Greetings and welcome!')
     res.render('index', {text: "lskdjf"})
 })
@@ -13,4 +13,9 @@ const userRouter = require("./routes/users.js")
 
 app.use('/users', userRouter)
 
+// It's middleware time!
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
 app.listen(3000) // listen for requests on port 3000
